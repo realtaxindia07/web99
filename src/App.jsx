798f0bbacfe334df80c3d445ff564bb0de417965
realtaxindia07 <realtaxindia07@gmail.com>
2025-06-16 18:47:01 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,12 @@ function App() {
 
       <Suspense fallback={<div>Loading footer...</div>}>
         <Footer />
+        {/* <!-- Elfsight AI Chatbot | Untitled AI Chatbot --> */}
+<script src="https://static.elfsight.com/platform/platform.js" async></script>
+<div className="elfsight-app-f3911cbf-692e-4fdc-b479-10f0adb57c62" data-elfsight-app-lazy></div>
+{/* <!-- Elfsight WhatsApp Chat | Untitled WhatsApp Chat --> */}
+<script src="https://static.elfsight.com/platform/platform.js" async></script>
+<div className="elfsight-app-dd2938bc-7e11-4e34-996d-413794ac47fb" data-elfsight-app-lazy></div>
       </Suspense>
     </>
   );
